02553043f98bcad14a3dd048720a9c4fc93f6dfc
abid ansari <abidraza8104@gmail.com>
2023-06-17 18:40:00 +0530

    audio feature added

--- javascript.js
+++ javascript.js
@@ -1,3 +1,11 @@
+const hamburgercontainer=document.querySelector("#header__main-nav")
+const hamburger=document.querySelector(".header__main-nav--hamburger")
+const links=document.querySelector(".header__main-nav--links li")
+
+hamburger.addEventListener("click",()=>{
+  hamburgercontainer.classList.toggle("clicked")
+})
+
 function AboutSection(ids){
     const aboutSection = document.getElementById(ids);
     const sectionTop = aboutSection.offsetTop;
@@ -61,3 +69,90 @@ function FormSubmitHandler(event) {
     });
   });
   
+
+  const musicImage = document.getElementById("meusic");
+  const audio = new Audio("free-juice-wrld-type-beat-imperfect-y2bs.com.mp3");
+  let isPlaying = false;
+  
+  function playAudio() {
+    audio.play();
+    isPlaying = true;
+  }
+  
+  function pauseAudio() {
+    audio.pause();
+    isPlaying = false;
+  }
+  
+  function toggleAudio() {
+    if (isPlaying) {
+      pauseAudio();
+    } else {
+      playAudio();
+    }
+  }
+  
+  musicImage.addEventListener("onmouseover", playAudio);
+  musicImage.addEventListener("click", toggleAudio);
+  musicImage.addEventListener("dblclick", toggleAudio);
+  
+  
+
+  document.getElementById("meusic").addEventListener("mouseenter",()=>{
+    playAudio()
+    document.getElementById("meusic_para").style.display="block"
+  })
+
+  document.getElementById("meusic").addEventListener("mouseleave",()=>{
+    document.getElementById("meusic_para").style.display="none"
+  })
+
+
+
+  function speakText(text) {
+    // Create a new SpeechSynthesisUtterance instance
+    const utterance = new SpeechSynthesisUtterance(text);
+    
+    // Speak the text
+    speechSynthesis.speak(utterance);
+  }
+  
+  // Example usage
+  
+  
+  document.getElementById("about").addEventListener("click",()=>{
+    speakText("about me")
+    return
+  })
+  document.getElementById("skills").addEventListener("click",()=>{
+    speakText("my skills")
+    return
+  })
+  document.getElementById("Project").addEventListener("click",()=>{
+    speakText("my all project")
+    return
+  })
+  
+  document.getElementById("service").addEventListener("click",()=>{
+    speakText("my services")
+    return
+  })
+  document.getElementById("hireme").addEventListener("click",()=>{
+    speakText("hire me")
+    return
+  })
+  document.getElementById("contact").addEventListener("click",()=>{
+    speakText("contact us")
+    return
+  })
+  document.getElementById("resume").addEventListener("click",()=>{
+    speakText("thank you for downloading my resume")
+  })
+
+  document.getElementById("openAlertButton").addEventListener("click",()=>{
+    speakText("Click Resume PDF Button TO Get Email and Contact Number.")
+  })
+
+  document.getElementById("okButton").addEventListener("click",()=>{
+    speakText("Thank You")
+  })
